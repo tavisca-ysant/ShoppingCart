edd242cbc22b7815dc89a645ea8de95da469e285
tavisca-ysant <ysant@tavisca.in>
2019-08-08 10:58:32 +0530

    Used a property for category in Product class

--- ShoppingCart/Product.cs
+++ ShoppingCart/Product.cs
@@ -3,17 +3,17 @@
     public class Product
     {
         public string Name { get;  }
-        private Category _category;
+        
         public double Price { get;  }
         public Category Category
         {
-            get => _category;
+            get;
         }
         public Product(string name, double price)
         {
             Name = name;
             Price = price;
-            _category = ProductCategoryFactory.GetCategory(Name);
+            Category = ProductCategoryFactory.GetCategory(Name);
         }
     }
 }
